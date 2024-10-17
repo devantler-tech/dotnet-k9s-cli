d1ac0b071e86bf96ff58a52176468208c87fc96c
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-17 07:00:47 +0200

    K9sCLI

--- .github/scripts/get-k9s-binary.sh
+++ .github/scripts/get-k9s-binary.sh
@@ -6,19 +6,28 @@ get() {
   local binary=$2
   local target_dir=$3
   local target_name=$4
-  local isTar=$5
+  local archiveType=$5
 
-  # check if tar
-  if [ "$isTar" = true ]; then
+  echo "Downloading $target_name from $url"
+  if [ "$archiveType" = "tar" ]; then
     curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
     mv "$target_dir/$binary" "${target_dir}/$target_name"
-  elif [ "$isTar" = false ]; then
+  elif [ "$archiveType" = "zip" ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name.zip"
+    unzip "$target_dir/$target_name.zip" -d "$target_dir"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+    rm "$target_dir/$target_name.zip"
+  elif [ "$archiveType" = false ]; then
     curl -LJ "$url" -o "$target_dir/$target_name"
   fi
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/derailed/k9s?os=darwin&arch=amd64" "k9s" "src/Devantler.K9sCLI/assets/binaries" "k9s-darwin-amd64" true
-get "https://getbin.io/derailed/k9s?os=darwin&arch=arm64" "k9s" "src/Devantler.K9sCLI/assets/binaries" "k9s-darwin-arm64" true
-get "https://getbin.io/derailed/k9s?os=linux&arch=amd64" "usr/bin/k9s" "src/Devantler.K9sCLI/assets/binaries" "k9s-linux-amd64" true
-get "https://getbin.io/derailed/k9s?os=linux&arch=arm64" "usr/bin/k9s" "src/Devantler.K9sCLI/assets/binaries" "k9s-linux-arm64" true
+get "https://getbin.io/derailed/k9s?os=darwin&arch=amd64" "k9s" "Devantler.K9sCLI/runtimes/osx-x64/native" "k9s-osx-x64" "tar"
+get "https://getbin.io/derailed/k9s?os=darwin&arch=arm64" "k9s" "Devantler.K9sCLI/runtimes/osx-arm64/native" "k9s-osx-arm64" "tar"
+get "https://getbin.io/derailed/k9s?os=linux&arch=amd64" "usr/bin/k9s" "Devantler.K9sCLI/runtimes/linux-x64/native" "k9s-linux-x64" "tar"
+rm -rf "Devantler.K9sCLI/runtimes/linux-x64/native/usr"
+get "https://getbin.io/derailed/k9s?os=linux&arch=arm64" "usr/bin/k9s" "Devantler.K9sCLI/runtimes/linux-arm64/native" "k9s-linux-arm64" "tar"
+rm -rf "Devantler.K9sCLI/runtimes/linux-arm64/native/usr"
+get "https://getbin.io/derailed/k9s?os=windows&arch=amd64" "k9s.exe" "Devantler.K9sCLI/runtimes/win-x64/native" "k9s-win-x64.exe" "zip"
+get "https://getbin.io/derailed/k9s?os=windows&arch=arm64" "k9s.exe" "Devantler.K9sCLI/runtimes/win-arm64/native" "k9s-win-arm64.exe" "zip"
